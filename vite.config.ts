89e34bb28db72b31ee98c66d491e97056ca79ae0
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-10-18 17:16:10 +0800

    feat(init-element-plus)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,81 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { terser } from 'rollup-plugin-terser'
+const path = require('path')
 
-// https://vitejs.dev/config/
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml (html) {
+      return html.replace(
+        /<title>(.*?)<\/title>/,
+        '<title>UPro</title>'
+      )
+    }
+  }
+}
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    htmlPlugin(),
+    terser(
+      {
+        compress: {
+          drop_console: true
+        }
+      }
+    )
+  ],
+  server: {
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://172.25.100.96/swgeneralplatform/api',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      },
+      {
+        find: 'store',
+        replacement: path.resolve(__dirname, 'src/store')
+      },
+      {
+        find: 'comps',
+        replacement: path.resolve(__dirname, 'src/components')
+      },
+      {
+        find: 'utils',
+        replacement: path.resolve(__dirname, 'src/utils')
+      },
+      {
+        find: 'modules',
+        replacement: path.resolve(__dirname, 'src/modules')
+      },
+      {
+        find: 'router',
+        replacement: path.resolve(__dirname, 'src/router')
+      },
+      {
+        find: 'widgets',
+        replacement: path.resolve(__dirname, 'src/widgets')
+      },
+      {
+        find: 'mixins',
+        replacement: path.resolve(__dirname, 'src/mixins')
+      }
+    ]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@import './src/styles/element-variables.scss';"
+      }
+    }
+  }
 })

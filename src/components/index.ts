7d1162e2fe2ac046826e5d29b16cb80a23e05506
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-10-21 16:02:53 +0800

    feat(type)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,8 @@
 // Vue Component Install
 import IconFont from 'comps/IconFont/index.vue'
-import test from 'modules/test.vue'
-console.log(IconFont)
+
 const component = {
   install: function (app: import("vue").App<any>): void {
-    console.log(IconFont)
-    console.log(test)
     app.component(IconFont.name, IconFont)
   }
 }
